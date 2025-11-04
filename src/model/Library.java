6be4ba9e4f6201bb4d1d2a5e753d0e6b71021b65
srmp47 <srmpoor47@gmail.com>
2025-11-04 18:15:46 +0330

    refactor(v1.2): change books data type from ArrayList to a generic LinkedList

--- src/model/Library.java
+++ src/model/Library.java
@@ -1,30 +1,50 @@
 package model;
 
-import java.util.ArrayList;
+import model.data_structures.GenericLinkedList;
+
 import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class Library {
-    private final ArrayList<Book> books = new ArrayList<Book>();
-    public ArrayList<Book> getBooks() {
+    private final GenericLinkedList<Book> books = new GenericLinkedList<>();
+
+    public GenericLinkedList<Book> getBooks() {
         return books;
     }
-    public void AddBook(Book book){
+
+    public void addBook(Book book) {
         this.books.add(book);
     }
-    public void removeBook(Book book){
+
+    public void removeBook(Book book) {
         books.remove(book);
     }
-    public ArrayList<Book> sortBooks(){
-        ArrayList<Book> sortedBooks = books;
+
+    public GenericLinkedList<Book> sortBooks() {
+        GenericLinkedList<Book> sortedBooks = new GenericLinkedList<>();
+
+        for (Book book : books) {
+            sortedBooks.add(book);
+        }
+
         sortedBooks.sort(Comparator.comparing(Book::getPublishDate).reversed());
         return sortedBooks;
     }
-    public ArrayList<Book> search(String keyword){
-        return books.stream()
+
+    public GenericLinkedList<Book> search(String keyword) {
+        return books.filter(book ->
+                book.getTitle().toLowerCase().contains(keyword.toLowerCase()) ||
+                        book.getAuthor().toLowerCase().contains(keyword.toLowerCase())
+        );
+    }
+
+    public GenericLinkedList<Book> searchWithStream(String keyword) {
+        GenericLinkedList<Book> result = new GenericLinkedList<>();
+        books.stream()
                 .filter(book ->
                         book.getTitle().toLowerCase().contains(keyword.toLowerCase()) ||
-                        book.getAuthor().toLowerCase().contains(keyword.toLowerCase()))
-                .collect(Collectors.toCollection(ArrayList::new));
+                                book.getAuthor().toLowerCase().contains(keyword.toLowerCase()))
+                .forEach(result::add);
+        return result;
     }
 }
